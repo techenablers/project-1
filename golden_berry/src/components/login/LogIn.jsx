ed790ec0447480ea814a7fa8f1e435abf2d7d88f
muralidhara_b <muralidhara.b@hidini.io>
2024-06-20 00:21:52 +0530

    style changes

--- golden_berry/src/components/login/LogIn.jsx
+++ golden_berry/src/components/login/LogIn.jsx
@@ -31,72 +31,74 @@ const LogIn = (props) => {
     console.log("i am logging in out ");
   };
   return (
-    <div className="modal-dialog modal-dialog-centered login-container">
-      <div className="model-close-icon">
-        <FontAwesomeIcon
-          icon={faTimes}
-          className="font-icon-x"
-          onClick={handleClose}
-        />
-      </div>
-      <div className="col-12 col-lg-6 col-md-6 col-sm-6">
-        <FontAwesomeIcon
-          icon={faLock}
-          style={{ color: "#E5881B", fontSize: "50px" }}
-        />
-        <h1 style={{ color: "#E5881B " }}>LOGIN</h1>
-        <form
-          style={{
-            textAlign: "left",
-            marginLeft: "20px",
-            position: "relative",
-            top: "10PX",
-          }}
-        >
-          <label style={{ color: "#fff" }}>USERNAME </label>
-          <input
-            type="text"
-            onChange={handleChange}
-            value={loginDetails.userName}
-            name="userName"
-            style={{backgroundColor:'#ced4da'}}
-          />
-          <label style={{ color: "#fff" }}>PASSWORD </label>
-          <input
-            type="password"
-            onChange={handleChange}
-            value={loginDetails.password}
-            name="password"
-            style={{backgroundColor:'#ced4da'}}
-          />
-          <button className="join-now" onSubmit={handleLogin}>
-            LOGIN
-          </button>
-        </form>
-        <div
-          style={{
-            alignSelf: "left",
-            position: "relative",
-            textAlign: "left",
-            top: "40px",
-            marginLeft: "20px",
-            color: "#E5881B",
-          }}
-        >
-          <p>Forget Password ?</p>
-          <p>Legal Information</p>
+    <div className="modal">
+      <div className="modal-content">
+        <div className="modal-header">
+          <span className="closeBtn" onClick={handleClose}>
+            &times;
+          </span>
         </div>
-      </div>
-      <div
-        className="col-12 col-lg-6 col-md-6 col-sm-6"
-        style={{ position: "relative", marginBottom: "130px" }}
-      >
-        <FontAwesomeIcon
-          icon={faKey}
-          style={{ color: "#E5881B", fontSize: "50px" }}
-        />
-        <h1 style={{ color: "#E5881B " }}>NEW MEMBER?</h1>
-        <button className="join-now"> JoinNow</button>
+        <div className="modal-body login-body"  >
+          <div className="modal-section">
+            {" "}
+            <FontAwesomeIcon
+              icon={faLock}
+              style={{ color: "#E5881B", fontSize: "50px" }}
+            />
+            <h1 style={{ color: "#E5881B " }}>LOGIN</h1>
+            <form
+              style={{
+                display:'flex',
+                flexDirection:'column',
+                textAlign: "left",
+                marginLeft: "20px",
+                position: "relative",
+                top: "10PX",
+              }}
+            >
+              <label style={{ color: "#fff" }}>USERNAME </label>
+              <input
+                type="text"
+                onChange={handleChange}
+                value={loginDetails.userName}
+                name="userName"
+                style={{ backgroundColor: "#ced4da" }}
+              />
+              <label style={{ color: "#fff" }}>PASSWORD </label>
+              <input
+                type="password"
+                onChange={handleChange}
+                value={loginDetails.password}
+                name="password"
+                style={{ backgroundColor: "#ced4da" }}
+              />
+              <button className="join-now" onSubmit={handleLogin}>
+                LOGIN
+              </button>
+            </form>
+            <div
+              style={{
+                alignSelf: "left",
+                position: "relative",
+                textAlign: "left",
+                top: "40px",
+                marginLeft: "20px",
+                color: "#E5881B",
+              }}
+            >
+              <p>Forget Password ?</p>
+              <p>Legal Information</p>
+            </div>
+          </div>
+          <div className="modal-section">
+            <FontAwesomeIcon
+              icon={faKey}
+              style={{ color: "#E5881B", fontSize: "50px" }}
+            />
+            <h1 style={{ color: "#E5881B ", width:'200px' }}>NEW MEMBER?</h1>
+            <button className="join-now"> JoinNow</button>
+          </div>
+        </div>
       </div>
     </div>
   );
